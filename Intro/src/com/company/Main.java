7ce79df7768a643ab8cabc51fcafd37d920a5d7f
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-10-12 21:43:41 +0600

    commit --a Intro

---
+++ Intro/src/com/company/Main.java
@@ -0,0 +1,175 @@
+package com.company;
+import com.company.models.*;
+//import com.company.models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        /*List<Integer> integers = new ArrayList<>();
+        List<Double> integers2 = new ArrayList<>();*/
+
+        Student student = new Student();
+        Person person = new Person();
+
+//        int[][] arr = {{1,2,3},{1,2,3},{1,3,4,5}};
+
+        /*int[] arr = new int[10];
+        int arr2[] = new int[10];
+        int arr3[] = {1, 2, 3, 4, 5};
+        int arr4[] = new int[] {1, 2, 3, 4, 5};*/
+
+        /*System.out.println(1);
+        for (; ;) {
+            System.out.println(2);
+        }
+        System.out.println(3);*/
+
+       /*int index = 0;
+       boolean b = true;
+
+       do {
+           if (b) {
+               index--;
+               System.out.println(index);
+           } else {
+               index++;
+           }
+           b = !b;
+       } while (index < 10);*/
+
+       /* char c = 'w';
+        System.out.println(1);
+        switch (c) {
+            case 'w':
+                System.out.println("a");
+
+            case 'b':
+                System.out.println("b");
+
+            case 'c':
+                System.out.println("c");
+            default:
+                System.out.println("default");
+        }
+        System.out.println(2);*/
+
+/*int x = 2;
+        if (true) {
+            System.out.println(1);
+        } else if (true) {
+            System.out.println(2);
+        } else if (true) {
+            System.out.println(3);
+        }
+        System.out.println(x);*/
+
+        /*int x = 5;
+if (x++>10 && ++x<0);
+
+        System.out.println(x);*/
+
+        /*int x = 10;
+        boolean b = true;
+        if (!b==false || x++ > 1) {
+            System.out.println("Da");
+        } else {
+            System.out.println("Net");
+        }
+        System.out.println(x);*/
+
+//        List<Integer> arr = new ArrayList<Integer>();
+        //List<int> arr = new List<int>();
+
+        /*Integer x;
+        Short x;
+        Double x;
+        Boolean x;
+        Float x;
+        Long x;
+        Character x;
+        Byte x;
+        String x;*/
+
+        /*byte a = 1;
+        long b = 1;
+        double c = 1.3;
+        float d = 1.3f;
+
+        System.out.println("sssssssss" +
+                "ssssssss" +
+                "ssssssss");
+
+        String str = """
+                a
+                a
+                a
+                a
+                """;
+        System.out.println(str);*/
+
+        /*int x = 0x6f;
+        int y = 010;
+        int z = 001001;
+        int a = 1_000_000;
+        System.out.println(a);*/
+
+
+        /*
+        boolean
+        byte - 128 127 ( 1 byte)
+        short -32k 32k (2 byte)
+        int (-2mlr 2mlrd) (4 byte)
+        long  (-xxxx xxxx) (8 byte)
+        double
+        float
+        char
+         */
+
+//      final int COUNT = 0;
+
+        /*int x, y, z;
+        x=y=z=10;
+        String str = "Vladimir";
+
+        final int a = 10;*/
+
+//        var f = 10;
+
+        /*dddd
+         * d
+         * d
+         * d
+         * d
+         * d
+         * dd
+         * */
+//
+//
+//        ssss
+//                s
+//                ss
+//                        s
+//                        s
+//        shorts
+//                s
+//                ss
+//
+        //ctrl + l
+//        shorts
+//                s
+//                ss
+//aaa
+//        System.out.print("Hello \n");
+//        System.out.print("Hello \t");
+//        System.out.print("Hello");
+//        System.out.print("Hello");
+//        for (int i = 0; i < args.length; i++) {
+//            System.out.print(args[i]);
+//        }
+    }
+}
---
+++ Intro/src/com/company/models/Student.java
@@ -0,0 +1,41 @@
+package com.company.models;
+
+public class Student {
+    String firstName;
+    String lastName;
+    int age;
+
+  public  Student() {
+
+    }
+
+    public Student(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
