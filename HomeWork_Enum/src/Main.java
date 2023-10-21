ed6d5030732b3b5007c70b0146d09ce3e398088a
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-10-21 16:03:14 +0600

    commit --a Home_Work_Enum DONE

--- HomeWork_Enum/src/Main.java
+++ HomeWork_Enum/src/Main.java
@@ -15,6 +15,9 @@ class Warrior implements Attack {
     public Warrior(String name, int hp) {
         this.name = name;
         this.hp = hp;
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
     }
 
     public String getName() {
@@ -71,7 +74,32 @@ HEAL {
     public boolean isAlive() {
         return true;
     }
-};
+},
+    SUPERATTACK {
+        @Override
+        public void modifyHp(Warrior warrior) {
+            warrior.modifyHp(new Warrior(warrior.getName(), warrior.getHp() - 100));
+            System.out.println(warrior.getName() + " теряет " + 100 + " hp, осталось " + warrior.getHp() + " hp");
+        }
+
+        @Override
+        public boolean isAlive() {
+            return false;
+        }
+    },
+
+    SUPPERHEAL {
+        @Override
+        public void modifyHp(Warrior warrior) {
+            warrior.modifyHp(new Warrior(warrior.getName(), warrior.getHp() + 50));
+            System.out.println(warrior.getName() + " исцеляет " + 50 + " hp, осталось " + warrior.getHp() + " hp");
+        }
+
+        @Override
+        public boolean isAlive() {
+            return true;
+        }
+    }
 
 }
 
@@ -98,27 +126,45 @@ class Battle implements Gaming {
 
         Random random = new Random();
         int attackerIndex = random.nextInt(count);
-        int loozerIndex;
+        int userIndex;
         do {
-            loozerIndex = random.nextInt(count);
-        } while (loozerIndex == attackerIndex);
+            userIndex = random.nextInt(count);
+        } while (userIndex == attackerIndex);
 
         Warrior attacker = attacks.get(attackerIndex);
-        Warrior loozer = attacks.get(loozerIndex);
+        Warrior user = attacks.get(userIndex);
 
-        fight(attacker, loozer);
+        fight(attacker, user);
     }
 
-    private void fight(Warrior attacker, Warrior loozer) {
+    private void fight(Warrior attacker, Warrior user) {
         int hp = new Random().nextInt(100) +1;
-        loozer.modifyHp(new Warrior(loozer.getName(), loozer.getHp() - hp));
-        System.out.println(attacker.getName() + " атакует " + loozer.getName() + " на " + hp +" hp, осталось ("
-                + loozer.getHp() + " hp)");
+        user.modifyHp(new Warrior(user.getName(), user.getHp() - hp));
+        System.out.println(attacker.getName() + " атакует " + user.getName() + " на " + hp +" hp, осталось ("
+                + user.getHp() + " hp)");
 
-        if (!loozer.isAlive()) {
-            attacks.remove(loozer);
-            System.out.println(loozer.getName() + " был побежден!");
+        if (!user.isAlive()) {
+            attacks.remove(user);
+            System.out.println(user.getName() + " был побежден!");
         }
+
+        if (user.getHp() < 30 && user.isAlive() && user.getAttackType() == AttackType.HEAL) {
+            user.setAttackType(AttackType.HEAL);
+        }
+
+        if (user.getHp() > 150 && user.isAlive() && user.getAttackType() == AttackType.DAMAGE) {
+            user.setAttackType(AttackType.DAMAGE);
+        }
+
+        if (user.isAlive() && user.getAttackType() == AttackType.SUPERATTACK) {
+            user.setAttackType(AttackType.SUPERATTACK);
+
+        }
+
+        if (user.isAlive() && user.getAttackType() == AttackType.SUPPERHEAL) {
+            user.setAttackType(AttackType.SUPPERHEAL);
+        }
+
         selected();
     }
 }
@@ -129,17 +175,27 @@ Warrior warrior = new Warrior("Abroxas", 300);
 Warrior warrior2 = new Warrior("Zorg", 300);
 Warrior warrior3 = new Warrior("Zena", 250);
 Warrior warrior4 = new Warrior("Velma", 250);
+Warrior warrior5 = new Warrior("Titan", 1000);
 
 Battle battle = new Battle();
 battle.addAttack(warrior);
 battle.addAttack(warrior2);
 battle.addAttack(warrior3);
 battle.addAttack(warrior4);
+battle.addAttack(warrior5);
+
 
 warrior.setAttackType(AttackType.DAMAGE);
+warrior2.setAttackType(AttackType.DAMAGE);
+warrior3.setAttackType(AttackType.DAMAGE);
+warrior4.setAttackType(AttackType.DAMAGE);
+warrior5.setAttackType(AttackType.SUPERATTACK);
+
+warrior.setAttackType(AttackType.HEAL);
 warrior2.setAttackType(AttackType.HEAL);
 warrior3.setAttackType(AttackType.HEAL);
-warrior4.setAttackType(AttackType.DAMAGE);
+warrior4.setAttackType(AttackType.HEAL);
+warrior5.setAttackType(AttackType.SUPPERHEAL);
 
 battle.selected();
     }
