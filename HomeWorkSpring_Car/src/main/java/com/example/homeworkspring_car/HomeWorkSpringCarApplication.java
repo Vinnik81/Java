817d87eacce4cc864cc8725a315b98ccf21e83c2
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-11-25 00:12:36 +0600

    commit --a HomeWorkSpring_Car Enter var1 & var2

--- HomeWorkSpring_Car/src/main/java/com/example/homeworkspring_car/HomeWorkSpringCarApplication.java
+++ HomeWorkSpring_Car/src/main/java/com/example/homeworkspring_car/HomeWorkSpringCarApplication.java
@@ -7,11 +7,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class HomeWorkSpringCarApplication {
 
     public static void main(String[] args) {
+        System.out.println("Введите первую машину (car1; car2; car3; car4): ");
+        String var1 = new java.util.Scanner(System.in).nextLine();
+        System.out.println("Введите вторую машину (car1; car2; car3; car4): ");
+        String var2 = new java.util.Scanner(System.in).nextLine();
+
+
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(HomeWorkSpringCarConfig.class);
-        Car car = context.getBean("car3", Car.class);
+        Car car = context.getBean(var1, Car.class);
         System.out.println(car);
         System.out.println();
-        Car car1 = context.getBean("car4", Car.class);
+        Car car1 = context.getBean(var2, Car.class);
         System.out.println(car1);
         context.close();
     }
